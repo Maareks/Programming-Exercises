cc5c9eb3bce129dac57660b28ad2cc68af1463c7
Maareks <marek.petrykowski@4experience.co>
2016-07-25 22:24:39 +0200

    no message

---
+++ Program1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+
+namespace Program1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			
+
+
+
+
+			int pinNumber = 1234;
+			int operatorPinNumber = 4321;
+			int currentBalance = 1000;
+			int atmMoney = 800;
+		Start:
+			Console.WriteLine("Type:\n1 - User\n2 - Operator");
+			int option = Convert.ToInt32(Console.ReadLine());
+			switch (option)
+			{
+				case 1:
+					Console.Write("Enter your PIN number: ");
+					int typedPinNumber = Convert.ToInt32(Console.ReadLine());
+					if (typedPinNumber != pinNumber)
+					{
+						Console.WriteLine("Wpisałeś błędny PIN, twoja karta została zablokowana do momentu odblokowania jej przez bank");
+						Console.ReadKey();
+					}
+					else 
+					{
+						while (true)
+						{
+							Console.WriteLine("Type:\n1 - display current balance\n2 - withdraw money\n3 - charge the card with the money inserted into ATM\n4 - LogOut");
+							int choosenOption = Convert.ToInt32(Console.ReadLine());
+							switch (choosenOption)
+							{
+								case 1:
+									Console.WriteLine("Your current balance is: " + currentBalance + " $");
+
+									break;
+								case 2:
+									Console.WriteLine("How much Do you want to withdraw?");
+									int withdrawedMoney = Convert.ToInt32(Console.ReadLine());
+									if (withdrawedMoney > atmMoney)
+									{
+										Console.WriteLine("Nie ma tyle pieniędzy w bankomacie.");
+									}
+									else if (withdrawedMoney > currentBalance)
+									{
+										Console.WriteLine("Nie masz tylu środków na koncie");
+									}
+									else
+									{
+										currentBalance = currentBalance - withdrawedMoney;
+										atmMoney = atmMoney - withdrawedMoney;
+										Console.WriteLine("Wybrałes " + withdrawedMoney + " $");
+										CreateLog("Withdraw", withdrawedMoney);
+
+									}
+									break;
+								case 3:
+									Console.WriteLine("How much do You want to deposit?");
+									int depositedMoney = Convert.ToInt32(Console.ReadLine());
+									Console.WriteLine("Insert money and press Enter");
+
+									currentBalance = currentBalance + depositedMoney;
+									atmMoney = atmMoney + depositedMoney;
+									CreateLog("Deposit", depositedMoney);
+
+
+									break;
+								case 4:
+									goto Start;
+
+							}
+
+						}
+					}
+				break;
+				case 2:
+					Console.Write("Put cashbox into ATM and enter Operator PIN number: ");
+					int typedOperatorPinNumber = Convert.ToInt32(Console.ReadLine());
+					if (typedOperatorPinNumber != operatorPinNumber)
+					{
+						Console.WriteLine("Wpisałeś błędny PIN");
+						Console.ReadKey();
+					}
+					else
+					{
+						atmMoney = atmMoney + 1000;
+						Console.WriteLine("1000$ has been added to ATM.");
+						CreateLog("Operator puts money", 1000);
+						goto Start;
+					}
+				break;
+			}
+		}
+
+	
+
+		public static void Log( TextWriter w, string operation, int amount)
+		{
+			w.Write("\r\nLog Entry : ");
+			w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+				DateTime.Now.ToLongDateString());
+			w.WriteLine("  :{0}", operation);
+			w.WriteLine("  :{0}", amount);
+			w.WriteLine("-------------------------------");
+		}
+
+		public static void CreateLog(string operation,int amount)
+		{
+			using (StreamWriter w = File.AppendText("log.txt"))
+			{
+				Log( w, operation,amount);
+			}
+		}
+	}
+}
---
+++ Program2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace program2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Person Andrzej = new Person();
+			Andrzej.AddTask(new Task("zadanie1"), 0);
+			Andrzej.AddTask(new Task("zadanie2"), 1);
+			Andrzej.AddTask(new Task("zadanie3"), 2);
+
+			Andrzej.AddTask(new Task("zadanie4"), 3);
+			Andrzej.tasks[3].Complete();
+			Andrzej.AddTask(new Task("zadanie5"), 4);
+			Andrzej.tasks[4].Complete();
+			Andrzej.AddTask(new Task("zadanie6"), 5);
+			Andrzej.tasks[5].Complete();
+			Andrzej.DisplayTasks();
+			Console.ReadKey();
+		}
+	}
+	class Person
+	{
+		public Task[] tasks = new Task[6];
+
+		public void AddTask(Task task, int taskNumber)
+		{
+			tasks[taskNumber] = task;
+		}
+		public void DisplayTasks()
+		{
+			foreach (Task number in tasks)
+			{
+				if (number.IsCompleted() == true)
+					Console.WriteLine(number.DisplayName());
+
+			}
+		}
+	}
+	class Task
+	{
+		string name;
+		bool completed;
+
+		public Task(string taskName)
+		{
+			name = taskName;
+			completed = false;
+		}
+
+		public string DisplayName()
+		{
+			return name;
+		}
+
+		public bool IsCompleted()
+		{
+			return completed;
+		}
+
+		public void Complete()
+		{
+			completed = true;
+		}
+	}
+
+}
